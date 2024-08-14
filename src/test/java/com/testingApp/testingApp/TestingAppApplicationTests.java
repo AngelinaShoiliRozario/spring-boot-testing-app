8218a1495e9e1c33374fec5eb7ccf0eb7400fd9f
Angelina_Shoili_Rozario <shoili.rozario@gmail.com>
2024-08-14 05:06:24 -0600

    initial commit

---
+++ src/test/java/com/testingApp/testingApp/TestingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testingApp.testingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
