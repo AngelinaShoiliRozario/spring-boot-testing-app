53c1ebac6b966db3869dcface97264897d5e8771
Angelina_Shoili_Rozario <shoili.rozario@gmail.com>
2024-08-15 01:51:35 -0600

    controller functions added

--- src/main/java/com/testingApp/testingApp/Controllers/EmployeeController.java
+++ src/main/java/com/testingApp/testingApp/Controllers/EmployeeController.java
@@ -10,6 +10,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -47,5 +49,10 @@ public class EmployeeController {
     @PutMapping("{id}")
     public Employee updateEmployee(@PathVariable long id, @RequestBody Employee employee) {
         return employeeService.updateEmployee(employee, id);
+    }
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteEmployee(@PathVariable long id){
+        employeeService.deleteEmployee(id);
+        return new ResponseEntity<>("The employee has been deleted successfully!", HttpStatus.OK);
     }
 }
